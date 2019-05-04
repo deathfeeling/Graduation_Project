138be8817d3c3242203b10bdd8373a579c69003e
haojie <haojie.lv@coloseo.cn>
2019-05-04 17:24:58 +0800

    完成电影分类部分

--- movie_pro/movies/urls.py
+++ movie_pro/movies/urls.py
@@ -4,9 +4,11 @@ from movies import views
 
 urlpatterns = [
     # path('create_moviedata_to_mysql/', views.create_moviedata_to_mysql),   #　创建数据到mysql
-    path('classification/', views.classification, name='classification'),  # 电影分类
+    path('classification/<int:classification_id>/', views.classification, name='classification'),  # 电影分类
     path('single/<int:id>/', views.single, name='single'),  # 电影详情
     path('news/', views.news, name='news'),   # 资讯
+    path('news_single/', views.news_single, name='news_single'),  # 资讯详情
     path('list/', views.list, name='list'),    # a-z表
     path('contact/', views.contact, name='contact'),    # 联系我
+    path('search/', views.search, name='search'),    # 搜索
 ]
--- movie_pro/movies/views.py
+++ movie_pro/movies/views.py
@@ -1,9 +1,11 @@
 import json
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from movies.models import *
+from user.views import top_movies
 
 
 def create_moviedata_to_mysql(request):
@@ -26,23 +28,46 @@ def create_moviedata_to_mysql(request):
         TbMovie.objects.create(title=title, actor=actor, language=language, director=director, release_date=release_date,
                                update_date=update_date, score=score, synopsis=synopsis, download_name=download_name,
                                download_size=download_size, download_thunder=download_thunder)
-
     return HttpResponse('Create successful!')
 
 
 def single(request, id):
     """电影详情"""
-    return render(request, 'single.html')
+    movie = TbMovie.objects.filter(m_id=id).first()
+    if movie:
+        movie_info = movie.__dict__
+        return render(request, 'single.html', movie_info)
+    return HttpResponse('Get some wrong!')
 
 
-def classification(request):
-    """电影分类"""
-    return render(request, 'classification.html')
+def classification(request, classification_id):
+    """电影分类 分页传入参数page"""
+    page = int(request.GET.get('page', 1))     # 页数
+    classification = TbClassification.objects.filter(id_classification=classification_id).first()
+    classification_name = classification.classification
+    movies = classification.tbmovie_set.all()
+    paginator = Paginator(movies, 10)   # 每页10条数据
+    items = paginator.page(page)
+    result = []
+    for item in items:
+        item = {'id': item.m_id, 'title': item.title,
+                'release_date': item.release_date, 'main_pic': item.main_pic}
+        result.append(item)
+    # 热门数据
+    top_movies_data = top_movies(8)
+    return render(request, 'classification.html', {'classification': classification_name, 'movies': result,
+                                                   'items': items, 'top_movies': top_movies_data,
+                                                   'classification_id': classification_id})
 
 
 def news(request):
     """资讯"""
     return render(request, 'news.html')
+
+
+def news_single(request):
+    """资讯详情"""
+    return render(request, 'news-single.html')
 
 
 def list(request):
@@ -53,3 +78,8 @@ def list(request):
 def contact(request):
     """联系我"""
     return render(request, 'contact.html')
+
+
+def search(request):
+    """搜索"""
+    return 'ok'
